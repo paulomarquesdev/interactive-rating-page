cbabeb8229883148582e019acc07a992ae464268
Paulo Marques <paulomarques.rs1@gmail.com>
2022-10-14 21:21:17 -0300

    Atualização - Mobile

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function appHeight() {
+        const doc = document.documentElement
+        doc.style.setProperty('--vh', (window.innerHeight*.01) + 'px');
+    }
+  
+window.addEventListener('resize', appHeight);
+appHeight();
+  
+
 const buttonSubmit = document.querySelector('.button-submit')
 
 buttonSubmit.addEventListener('click', () => {
